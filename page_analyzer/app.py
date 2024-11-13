5edae10c8c86b45094b58ffd51a3d15ebca6556a
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-11-13 18:10:16 +0500

    Changes app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,16 +1,24 @@
+import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
 
 
 load_dotenv()
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = SECRET_KEY
+app.secret_key = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    """Return the main page."""
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
