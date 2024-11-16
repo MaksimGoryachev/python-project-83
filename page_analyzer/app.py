ea33d73f89cc0dccbe9a782c13b643c67d88ba32
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-11-16 19:59:58 +0500

    Update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -15,18 +15,29 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
+
 conn = psycopg2.connect(DATABASE_URL)
+
+# try:
+#     conn = psycopg2.connect(DATABASE_URL)
+# except Exception as e:
+#     print(f"Ошибка подключения к базе данных: {e}")
 
 
 @app.route('/')
 def index():
     """Return the main page."""
+    return render_template('base.html')
+
+
+@app.get('/urls')
+def urls():
     return render_template('urls.html')
 
 
-@app.route('/urls')
-def urls():
-    return render_template('index.html')
+@app.get('/urls/<int:url_id>')
+def url(url_id):
+    return render_template('url.html')
 
 
 if __name__ == '__main__':
